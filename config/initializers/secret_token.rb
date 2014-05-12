aff179c297cbffa9e4e13fb951c912738e5efa05
Ricardo Rubio <rickarubio@gmail.com>
2014-05-11 18:34:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizlyme::Application.config.secret_token = '0d53bf052f3d37d75792d0810f3c4355c973222813c5c7af7d5ec4bdff3c256df1013a9c4e20f7ec37234ecd3d82f0fe68019fb8ed9dd21170187a7fa79f0c9c'
