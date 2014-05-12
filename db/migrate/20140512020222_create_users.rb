2511dac5f132f9c2c5270f5beb4e5623b43efd8d
Ricardo Rubio <rickarubio@gmail.com>
2014-05-11 19:22:19 -0700

    implements users migration, adds bcrypt-ruby for use of #has_secure_password

--- db/migrate/20140512020222_create_users.rb
+++ db/migrate/20140512020222_create_users.rb
@@ -1,7 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
-  def up
-  end
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
 
-  def down
+      t.timestamps
+    end
   end
 end
