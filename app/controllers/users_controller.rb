d077abda09f07cbb40790eb7e8e3a2b72fa9075a
Ricardo Rubio <rickarubio@gmail.com>
2014-05-12 14:29:39 -0700

    adds SessionsHelper module and a navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       @user = User.new
       render :new
