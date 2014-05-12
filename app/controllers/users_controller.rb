10d4c1f39d3670df1b6ef7dc97d2dab798847229
Ricardo Rubio <rickarubio@gmail.com>
2014-05-11 22:53:00 -0700

    implements UsersController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      @user = User.new
+      render :new
+    end
   end
 
   def show
