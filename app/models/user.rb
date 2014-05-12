d4896dcf30949ef733c866a253b6c26d2c3797b0
Ricardo Rubio <rickarubio@gmail.com>
2014-05-12 13:36:14 -0700

    adds uniqueness validation to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :password
   validates_presence_of :username, :password
-  has_secure_password
+  validates :username, uniqueness: true
   has_many :results
   has_many :quizzes, through: :results
+  has_secure_password
 
   def self.authenticate(username, password)
     @user = find_by_username(username)
