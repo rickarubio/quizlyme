86f6ae04586352a768d436091ac882d978d2a819
Ricardo Rubio <rickarubio@gmail.com>
2014-05-18 15:21:45 -0700

    user can only select one answer choice per question

--- app/assets/javascripts/examination.js
+++ app/assets/javascripts/examination.js
@@ -6,8 +6,23 @@ var examination = (function() {
   return {
     isExamination: function() {
       if (document.location.pathname.match("/examination/")) {
-        console.log('exam in progress');
+        examination.markUserSelection();
       }
+    },
+    markUserSelection: function() {
+      $('.examination-choice').on('click', function() {
+        // visually mark the user's selected answer choice
+        var selectedChoice = $(this);
+
+        selectedChoice.css('background-color', 'rgba(27, 195, 248, 1.0)');
+
+        selectedChoice.parent().children().each(function(idx, choice) {
+          if (!$(choice).is(selectedChoice)) {
+            $(choice).css('background-color', '');
+          }
+        });
+
+      })
     }
   }
 })();
